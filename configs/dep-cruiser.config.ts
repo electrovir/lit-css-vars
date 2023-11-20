7ec0735fb18a286623e74b73e3a2bb40f4349f13
electrovir <electrovir@users.noreply.github.com>
2023-11-20 18:09:23 +0000

    [patch] allow updated lit peer dep

--- configs/dep-cruiser.config.ts
+++ configs/dep-cruiser.config.ts
@@ -1,16 +1,27 @@
 import type {IConfiguration} from 'dependency-cruiser';
 import {generateDepCruiserConfig} from 'virmator/dist/compiled-base-configs/base-dep-cruiser.config';
 
-const depCruiserConfig: IConfiguration = generateDepCruiserConfig({
+const baseConfig = generateDepCruiserConfig({
     fileExceptions: {
         // enter file exceptions by rule name here
-        'no-orphans': [
-            'src/index.ts',
-        ],
+        'no-orphans': {
+            from: [
+                'src/index.ts',
+            ],
+        },
+        'no-duplicate-dep-types': {
+            to: [
+                'lit',
+            ],
+        },
     },
     omitRules: [
         // enter rule names here to omit
     ],
 });
+
+const depCruiserConfig: IConfiguration = {
+    ...baseConfig,
+};
 
 module.exports = depCruiserConfig;
